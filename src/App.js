dc2ab7a7dc2b0c2b1e093720955e62ed0da6ff86
Kwaku Louis <bonglamgh@gmail.com>
2022-12-15 20:59:47 +0000

    added styles

--- src/App.js
+++ src/App.js
@@ -32,17 +32,17 @@ export default function App() {
 
   return (
     <div className="App  ">
-      <div className="flex justify-center items-center">
-      <p className="  mb-10  mt-32 md:mt-16 text-black text-2xl  font-bold py-2 px-4 ">  
+      <div className="flex justify-center items-center bg-slate-300">
+      <p className="  mb-10   mt-10 md:mt-16 text-black text-2xl  font-black py-2 px-4 ">  
          Date Calculator <i class="fa-solid fa-calculator text-3xl text-red-400 "></i>
         </p>
 
       </div>
             
 
-      <h1 className="text-base sm:text md:px-40 text-center px-8 md:text-sm lg:text-2xl font-semibold mb-6 ">This App Takes Date of Birth As the  Input and Returns the Difference in  <span className="text-green-600">Years</span> , <span className="text-green-600">Months</span>, and <span className="text-green-600">Days</span> as compared to the current date. </h1>
+      <h1 className="text-base bg-red-100 sm:text md:px-40 text-center px-8 py-6 md:text-sm lg:text-2xl font-semibold mb-6 ">This App Takes Date of Birth As the  Input and Returns the Difference in  <span className="text-green-600">Years</span> , <span className="text-green-600">Months</span>, and <span className="text-green-600">Days</span> as compared to the current date. </h1>
 
-      <form className="flex justify-center items-center">
+      <form className="flex justify-center  items-center">
       <label className="text-green-600" for="start">Date of Birth &nbsp; :&nbsp;&nbsp;  </label> <br/> 
       <input className="px-2 py-2 border border-red-300" type="date" placeholder=" Date Of Birth" value={date} onChange={handleChange} />
 
@@ -52,21 +52,22 @@ export default function App() {
       <br />
       
       <div className="mt-2 text-center">
-      <p> Date difference in  years: <span className="text-green-500"> {dateDiffInYears}</span></p>
+        <p className="bg-green-400 text-2xl py-2 font-black "> Date Differences:</p>
+      <p className="text-xl bg-gray-400 text-white font-serif px-4 py-4">  Years&nbsp;: <span className="text-green-500 text-2xl bg-black px-2 py-2 "> {dateDiffInYears}</span></p>
 
-       <p> Date difference in months: <span className="text-green-600">{dateDiffInMonths}</span> </p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-4 py-4"> Months&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2">{dateDiffInMonths}</span> </p>
 
-       <p> Date difference in days: <span className="text-green-600"> {dateDiffInDays}</span></p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-4 py-4"> Days&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInDays}</span></p>
 
-       <p> Date difference in minutes: <span className="text-green-600"> {dateDiffInMinutes}</span></p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-4 py-4"> Minutes&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInMinutes}</span></p>
 
-       <p> Date difference in secondss: <span className="text-green-600"> {dateDiffInSeconds}</span></p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-4 py-4"> Seconds&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInSeconds}</span></p>
       
       </div>
       
       <div className="flex justify-center items-center pt-10">
 
-      <button onClick={calculateDayFromBirth} className="bg-blue-500  text-white font-bold py-2 px-4 ">
+      <button onClick={calculateDayFromBirth} className="bg-green-600  text-white font-bold py-2 px-4 ">
         Generate Results
        </button>
      
