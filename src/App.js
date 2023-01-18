4147e100ee44de441afb7a800e8579c9a95ff280
Kwaku Louis <bonglamgh@gmail.com>
2023-01-18 19:13:36 +0000

    made UI changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import moment from "moment";
+import Button from '@mui/material/Button';
+import  "./index.css";
 
 
 export default function App() {
@@ -31,14 +33,14 @@ export default function App() {
   }
 
   return (
-    <div className="App  ">
+    <div className=" py-24 md:py-5   ">
      
             
 
-      <h1 className="text-base bg-red-100 sm:text md:px-40 text-center px-8 py-6 md:text-sm lg:text-2xl font-semibold mb-6 ">This App Takes Date of Birth As the  Input and Returns the Difference in  <span className="text-green-600">Years</span> , <span className="text-green-600">Months</span>, and <span className="text-green-600">Days</span> as compared to the current date. </h1>
+      <h1 className="text-sm text-center pt-10 pb-8 md:pt-10 md:pb-8 font-medium"> Enter your date of birth and generate the results.  </h1>
 
-      <form className="flex justify-center  items-center">
-      <label className="text-green-600" for="start">Date of Birth &nbsp; :&nbsp;&nbsp;  </label> <br/> 
+      <form className="flex justify-center   items-center">
+      <label className="text-black font-bold text-base" for="start">Date of Birth &nbsp;:&nbsp;&nbsp;  </label> <br/> 
       <input className="px-2 py-2 border border-red-300" type="date" placeholder=" Date Of Birth" value={date} onChange={handleChange} />
 
 
@@ -46,33 +48,68 @@ export default function App() {
 
       <br />
       
-      <div className="mt-2 text-center">
-        <p className="bg-green-400 text-lg py-2 font-black "> Date Differences:</p>
-      <p className="text-xl bg-gray-400 text-white font-serif px-2 py-2">  Years&nbsp;: <span className="text-green-500 text-2xl bg-black px-2 py-2 "> {dateDiffInYears}</span></p>
+      {/* <div className="mt-2 text-center">
+        <p className="bg-green-400 text-lg py-2 font-medium "> Date Differences:</p>
+      <p className="text-xl bg-gray-400 text-white font-serif px-2 py-3">  Years&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp; <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInYears}</span></p>
 
-       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-2"> Months&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2">{dateDiffInMonths}</span> </p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-3"> Months&nbsp;:&nbsp; <span className="text-green-600 text-2xl bg-black px-2 py-2">{dateDiffInMonths}</span> </p>
 
-       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-2"> Days&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInDays}</span></p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-3"> Days&nbsp;: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInDays}</span></p>
 
-       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-2"> Minutes&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInMinutes}</span></p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-3"> Minutes&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInMinutes}</span></p>
 
-       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-2"> Seconds&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInSeconds}</span></p>
+       <p className="text-xl bg-gray-400 text-white font-serif px-2 py-3"> Seconds&nbsp;: <span className="text-green-600 text-2xl bg-black px-2 py-2"> {dateDiffInSeconds}</span></p>
       
-      </div>
-      
-      <div className="flex justify-center items-center pt-10">
+      </div> */}
+    
 
-      <button onClick={calculateDayFromBirth} className="bg-green-600  text-white font-bold py-2 px-4 ">
-        Generate Results
-       </button>
-     
-      </div>
-      <p className="mt-8 text-center "> <span className="font-bold">How To Use</span> : Input your date of birth and click on generate results to display the results. </p>
+      {/* <div class="flex flex-row my-10 gap-4 justify-center items-center ">
+  <div className="bg-red-200 px-5 rounded-md py-5">  <p className="font-medium">  Years&nbsp;: <br/>  <br/>  <span className=" bg-cyan-100 rounded-md text-black px-4 py-2"> {dateDiffInYears}</span></p></div>
+  <div className="bg-red-200 rounded-md px-5 py-5">  <p className="font-medium"> Months&nbsp;:&nbsp; <br/>  <br/>   <span className="bg-cyan-100 rounded-md text-black px-4 py-2">{dateDiffInMonths}</span> </p></div>
+  <div className="bg-red-200  rounded-md  px-5 py-5">  <p className="font-medium"> Days&nbsp;:&nbsp;<br/>  <br/> <span className="bg-cyan-100 rounded-md text-black px-4 py-2">{dateDiffInDays}</span> </p></div>
+  
+</div>
+<div class="flex flex-row gap-4 justify-center items-center ">
+  <div className="bg-red-200 px-5 rounded-md py-5">  <p className="font-medium">  Minutes&nbsp;:<br/>  <br/>  <span className="bg-cyan-100 rounded-md text-black px-4 py-2"> {dateDiffInMinutes}</span></p></div>
+  <div className="bg-red-200 px-5  rounded-md py-5">  <p className="font-medium">Seconds&nbsp;:&nbsp;<br/>  <br/> <span className="bg-cyan-100 rounded-md text-black text-lg px-4 py-2">{dateDiffInSeconds}</span> </p></div>
 
-      <p className="text-center text-sm font-black py-4 text-blue-500"> Kwaku Louis&copy;</p>
-     
+  
+</div> */}
+  <div className="md:flex md:justify-center md:items-center">
+    
+<div className="grid grid-cols-3 px-2 pt-10 pb-5 md:grid-cols-4 gap-2">
+<div className="bg-red-200 px-5 rounded-md py-5">  <p className="font-medium">  Years&nbsp;: <br/>  <br/>  <span className=" bg-cyan-100 rounded-md text-black px-4 py-2"> {dateDiffInYears}</span></p></div>
+  <div className="bg-red-200 rounded-md px-5 py-5">  <p className="font-medium"> Months&nbsp;:&nbsp; <br/>  <br/>   <span className="bg-cyan-100 rounded-md text-black px-4 py-2">{dateDiffInMonths}</span> </p></div>
+  <div className="bg-red-200  rounded-md  px-5 py-5">  <p className="font-medium"> Days&nbsp;:&nbsp;<br/>  <br/> <span className="bg-cyan-100 rounded-md text-black px-4 py-2">{dateDiffInDays}</span> </p></div>
+
+
+
+
+</div>
+
+<div className="grid grid-cols-3  px-2  pt-8 md:grid-cols-3  gap-2 md:gap-6">
+<div className="bg-red-200 px-5 rounded-md py-5">  <p className="font-medium ">  Minutes&nbsp;:<br/>  <br/>  <span className="bg-cyan-100 rounded-md text-black px-4 py-2"> {dateDiffInMinutes}</span></p></div>
+  <div className="bg-red-200 px-5  rounded-md py-5">  <p className="font-medium">Seconds:<br/>  <br/> <span className="bg-cyan-100 rounded-md text-black text-lg px-4 py-2">{dateDiffInSeconds}</span> </p></div>
+
+</div>
+
+
+  </div>
+
+
+<div className="flex justify-center items-center pt-14">
+
+    
+<Button  onClick={calculateDayFromBirth}  variant="outlined">Generate Results</Button>
+
+</div>
+
+
+<p className="text-center text-sm  py-6 "> Kwaku Louis &copy;</p>
      
     </div>
+
+    
   );
 }
 
